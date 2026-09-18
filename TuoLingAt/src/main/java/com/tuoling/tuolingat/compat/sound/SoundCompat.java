7ae282b5f64fae7bae7adb5e6f105c6f20b12a63
TuoLingNine <2718479709@qq.com>
2026-09-19 01:04:11 +0800

    Delete TuoLingAt directory

--- TuoLingAt/src/main/java/com/tuoling/tuolingat/compat/sound/SoundCompat.java
+++
@@ -1,9 +0,0 @@
-package com.tuoling.tuolingat.compat.sound;
-
-import com.tuoling.tuolingat.compat.model.SoundModel;
-import org.bukkit.entity.Player;
-
-public interface SoundCompat {
-    boolean isMatch();
-    void sendSound(Player player, SoundModel model);
-}
